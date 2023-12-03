58de4f0b8c486e79da8215e1abb0ced3b026e841
hansonnhu <43202300+hansonnhu@users.noreply.github.com>
2023-12-03 19:03:46 +0800

    轉正finction功能提升

--- WinFormsApp1/obj/Debug/net6.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net6.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+501444c7286c1c83b79659a30bdb262f0cf0e807")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
